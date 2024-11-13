8af2e7ad734f868e66356f8b0767714a1ab9446b
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-11-13 22:51:10 +0000

    passworld impl was causing issues

--- password.php
+++ password.php
@@ -10,13 +10,35 @@ PHP'S BUILT-IN CYRPTOGRAPHIC FUNCTIONS, REJECTION SAMPLING TO REMOVE RANDOM BIAS
 AMONG OTHER PRE-REQUISITES THAT WILL MAKE THIS UTILITY A VERSATILE ALTERNATIVE
 */
 
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
+
 /* PRE-PROCESSOR DIRECTIVES */
 
 define('MAX_PASSWORD_LEN', 128);
 define('MIN_PASSWORD_LEN', 12);
 define('MULTI', 2);
 
-class Password implements password_impl
+// CHARACTER SET DEFINITIONS
+
+const UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const LOWER = 'abcdefghijklmnopqrstuvwxyz';
+const NUMS = '0123456789';
+const SYMS = '!@#$%^&*()-_=+[]';
+
+$HAS_UPPER = 1;
+$HAS_LOWER = 1;
+$HAS_NUMBER = 1;
+$HAS_SYMBOL = 1;
+$MIN_UPPER = 1;
+$MIN_LOWER = 1;
+$MIN_NUMBER = 1;
+$MIN_SYMBOL = 1;
+
+
+class Password
 {
     // JUST ACCESS THE SECURE BYTES OF THE RANDOMLY GENERATED HASH
 
@@ -96,15 +118,107 @@ class Password implements password_impl
     }
 
 
+    /* VALIDATES PASSWORD AGAINST REQUIREMENTS
+    *  ENSURES MINIMUM CHARACTER TYPE COUNTS ARE MET
+    *  @param $PASSWORD: PASSWORD TO VALIDATE
+    *  @param $LEN: PASSWORD LENGTH
+    *  @param $REQS: REQUIREMENTS OBJECT
+    *  @return: BOOLEAN INDICATING IF PASSWORD MEETS REQUIREMENTS
+    */
+
     public function IS_VALID_PASSWORD($PASSWORD, $LEN, $REQS)
     {
+        $UPPER_COUNT = preg_match_all('/[' . UPPER . ']/', $PASSWORD);
+        $LOWER_COUNT = preg_match_all('/[' . LOWER . ']/', $PASSWORD);
+        $NUMS_COUNT = preg_match_all('/[' . NUMS . ']/', $PASSWORD);
+        $SYMS_COUNT = preg_match_all('/[' . SYMS . ']/', $PASSWORD);
 
+        return $UPPER_COUNT >= $REQS->MIN_UPPER &&
+               $LOWER_COUNT >= $REQS->MIN_LOWER &&
+               $NUMS_COUNT >= $REQS->MIN_NUMS &&
+               $SYMS_COUNT >= $REQS->MIN_SYMS;
     }
+
+     /* MAIN PASSWORD GENERATION METHOD
+    *  CREATES SECURE PASSWORD MEETING ALL REQUIREMENTS
+    *  @param $LEN: DESIRED PASSWORD LENGTH
+    *  @param $REQS: REQUIREMENTS OBJECT
+    *  @return: GENERATED PASSWORD
+    *  @throws Exception ON INVALID LENGTH OR REQUIREMENTS
+    */
 
     public function GENERATE_PASSWORD($LEN, $REQS)
     {
+        /* VALIDATE PASSWORD LENGTH AGAINST CONSTRAINTS */
 
+        if ($LEN < MIN_PASSWORD_LEN || $LEN > MAX_PASSWORD_LEN)
+        {
+            throw new Exception("PASSWORD LENGTH MUST BE BETWEEN " . MIN_PASSWORD_LEN . " AND " . MAX_PASSWORD_LEN);
+        }
+
+        /* ENSURE LENGTH CAN ACCOMMODATE MINIMUM REQUIREMENTS */
+
+        $MIN_REQUIRED = $REQS->MIN_UPPER + $REQS->MIN_LOWER + 
+                       $REQS->MIN_NUMS + $REQS->MIN_SYMS;
+
+        if ($LEN < $MIN_REQUIRED)
+        {
+            throw new Exception("PASSWORD LENGTH TOO SHORT FOR REQUIREMENTS");
+        }
+
+        /* INITIALIZE ENTROPY POOL AND PASSWORD ARRAY */
+
+        $POOL = $this->GET_SECURE_BYTES($LEN * MULTI * 4);
+        $INDEX = 0;
+        $PASSWORD_ARRAY = [];
+
+        /* FULFILL MINIMUM REQUIREMENTS FOR EACH CHARACTER TYPE */
+
+        for ($I = 0; $I < $REQS->MIN_UPPER; $I++)
+        {
+            $PASSWORD_ARRAY[] = $this->GET_UNBIASED_CHAR(UPPER, $POOL, $INDEX);
+        }
+
+        for ($I = 0; $I < $REQS->MIN_LOWER; $I++)
+        {
+            $PASSWORD_ARRAY[] = $this->GET_UNBIASED_CHAR(LOWER, $POOL, $INDEX);
+        }
+
+        for ($I = 0; $I < $REQS->MIN_NUMS; $I++)
+        {
+            $PASSWORD_ARRAY[] = $this->GET_UNBIASED_CHAR(NUMS, $POOL, $INDEX);
+        }
+
+        for ($I = 0; $I < $REQS->MIN_SYMS; $I++)
+        {
+            $PASSWORD_ARRAY[] = $this->GET_UNBIASED_CHAR(SYMS, $POOL, $INDEX);
+        }
+
+        /* CONSTRUCT ALLOWED CHARACTER SET BASED ON REQUIREMENTS */
+
+        $CHARSET = '';
+        if ($REQS->HAS_UPPER) $CHARSET .= UPPER;
+        if ($REQS->HAS_LOWER) $CHARSET .= LOWER;
+        if ($REQS->HAS_NUMS) $CHARSET .= NUMS;
+        if ($REQS->HAS_SYMS) $CHARSET .= SYMS;
+
+        /* FILL REMAINING LENGTH WITH RANDOM CHARACTERS */
+
+        while (count($PASSWORD_ARRAY) < $LEN)
+        {
+            $PASSWORD_ARRAY[] = $this->GET_UNBIASED_CHAR($CHARSET, $POOL, $INDEX);
+        }
+
+        /* SHUFFLE AND VALIDATE FINAL PASSWORD */
+
+        $this->CRYPTO_SHUFFLE($PASSWORD_ARRAY);
+        $PASSWORD = implode('', $PASSWORD_ARRAY);
+
+        if (!$this->IS_VALID_PASSWORD($PASSWORD, $LEN, $REQS))
+        {
+            return $this->GENERATE_PASSWORD($LEN, $REQS);
+        }
+
+        return $PASSWORD;
     }
 }
-
-?>
