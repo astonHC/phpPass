a9ddafa4c3a3cae1e03a4566f1baf444a845ff28
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-11-13 20:40:58 +0000

    added modular interface

---
+++ password_impl.php
@@ -0,0 +1,21 @@
+<?php
+
+/* COPYRIGHT (C) HARRY CLARK 2024 */
+/* GENERIC ALL PURPOSE PHP PASSWORD GENERATOR */
+
+/* THE FOLLOWING FILE PERTAINS TOWARDS THE MODULARISATION OF LOGIC */
+
+interface password_impl
+{
+    public int $HAS_UPPER = 1;
+    public int $HAS_LOWER = 1;
+    public int $HAS_NUMBER = 1;
+    public int $HAS_SYMBOL = 1;
+    public int $MIN_UPPER = 1;
+    public int $MIN_LOWER = 1;
+    public int $MIN_NUMBER = 1;
+    public int $MIN_SYMBOL = 1;
+
+    public function GET_SECURE_BYTES($LEN);
+
+}
